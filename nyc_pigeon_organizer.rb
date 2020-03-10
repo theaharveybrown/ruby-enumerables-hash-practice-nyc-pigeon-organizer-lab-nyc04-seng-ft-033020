d1f310dd35fa86364797dda254894c2ff20cbeff
Anthea Harvey-Brown <tharveybrown@gmail.com>
2020-03-10 17:35:55 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,47 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
-end
+
+  pigeon_list = {}
+  data.each do |key, value|
+    value.each do |attribute, person|
+      
+      person.each do |n|
+        if !pigeon_list[n]
+          pigeon_list[n] = {}
+        end 
+        
+        
+        if !pigeon_list[n][key]
+          pigeon_list[n][key] = []
+        end 
+        pigeon_list[n][key] << attribute.to_s
+      end
+    
+    end
+  end
+  pigeon_list
+end 
+
+
+pigeon_data = {
+  :color => {
+    :purple => ["Theo", "Peter Jr.", "Lucky"],
+    :grey => ["Theo", "Peter Jr.", "Ms. K"],
+    :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+    :brown => ["Queenie", "Alex"]
+  },
+  :gender => {
+    :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+    :female => ["Queenie", "Ms. K"]
+  },
+  :lives => {
+    "Subway" => ["Theo", "Queenie"],
+    "Central Park" => ["Alex", "Ms. K", "Lucky"],
+    "Library" => ["Peter Jr."],
+    "City Hall" => ["Andrew"]
+  }
+}
+
+
+
+
+nyc_pigeon_organizer(pigeon_data)
